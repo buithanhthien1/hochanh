93d52287d84cc1f68e535530ee888e96f2c886d8
buithanhthien1 <65117531+buithanhthien1@users.noreply.github.com>
2020-05-10 19:46:36 +0700

    Add files via upload

---
+++ buithanhthien...py
@@ -0,0 +1,7 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, world")
+Hello, world
+>>> print("Bui Thanh Thien, mssv:18575202160041, 59K TĐH")
+Bui Thanh Thien, mssv:18575202160041, 59K TĐH
+>>> 
